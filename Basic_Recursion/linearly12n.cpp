c35a3620a245292194d6fb1b83c43e4fcb2fa4c0
ankit7115 <ankitsrivastav5555@gmail.com>
2023-05-28 01:53:34 +0530

    +1 added commit

--- Basic_Recursion/linearly12n.cpp
+++ Basic_Recursion/linearly12n.cpp
@@ -4,11 +4,15 @@ using namespace std;
 
 void print(int i,int n){
     if(i>n)return;
-    cout<<i<<" ";
+    cout<<i<<"\n";
     print(i+1,n);
 }
 
 int main(){
+    #ifndef ONLINEJUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
     int n;
     cin>>n;
     print(1,n);
--- Basic_Recursion/namePrint.cpp
+++ Basic_Recursion/namePrint.cpp
@@ -4,10 +4,15 @@ using namespace std;
 
 void print(string name,int n){
     if(n==0)return;
-    cout<<name<<" ";
+    cout<<name<<endl;
     print(name,n-1);
 }
 int main(){
+    #ifndef ONLINEJUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    
     int n;
     cin>>n;
     print("Ankit",n);
